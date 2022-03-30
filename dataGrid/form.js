5f2f56c3f5a7779ebf77eecb804d4979345b4bd2
safwen-code <safwendjebbi1234@gmail.com>
2022-03-30 15:55:30 +0100

    create a form and updated with current datafiltred

--- dataGrid/form.js
+++ dataGrid/form.js
@@ -1,23 +1,57 @@
 const etat = ['encour', 'cree', 'terminer']
+const tab2 = [
+  {
+    id: 1,
+    nomAction: 'a1',
+    PA: 'pa1',
+    date: '12/05/2022',
+    StateId: 1,
+  },
+  {
+    id: 2,
+    nomAction: 'a2',
+    PA: 'pa1',
+    date: '12/05/2022',
+    StateId: 1,
+  },
+  {
+    id: 3,
+    nomAction: 'a3',
+    PA: 'pa1',
+    date: '12/05/2022',
+    StateId: 1,
+  },
+]
 
 $(() => {
+  var idString = window.location.search.substring(1)
+  var idurl = idString.charAt(idString.length - 1)
+  console.log(idurl)
+  console.log(typeof idurl)
+  const ActionFind = tab2.filter((el) => el.id === parseInt(idurl))
+  console.log(ActionFind)
+
+  // for (item of ActionFind) {
+  //   console.log(item.nomAction)
+  // }
+
   const form = $('#form')
     .dxForm({
       // create data
       formData: {
-        NomAction: '',
+        nomAction: '',
         PA: '',
-        Date: '',
+        date: '',
         stateId: '',
       },
 
       colCount: 2,
       //configure item
       items: [
-        'NomAction',
+        'nomAction',
         'PA',
         {
-          dataField: 'Date',
+          dataField: 'date',
           editorType: 'dxDateBox',
         },
         {
@@ -32,6 +66,10 @@ $(() => {
       ],
     })
     .dxForm('instance')
+
+  //update data with new data filtred
+  form.option('formData', ActionFind[0])
+
   const button = $('#button').dxButton({
     text: 'Submit',
     type: 'success',
